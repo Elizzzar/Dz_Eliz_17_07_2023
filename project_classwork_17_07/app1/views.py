5af3a54f8ff903be06c899aef0cad88cfd460afb
Elizzzar <125677273+Elizzzar@users.noreply.github.com>
2023-07-17 21:46:13 +0600

    Update views.py

--- project_classwork_17_07/app1/views.py
+++ project_classwork_17_07/app1/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
     data = [x for x in range(1, 10)]
 
-    return HttpResponse(data, safe = False)
+    return JsonResponse(data, safe = False)
